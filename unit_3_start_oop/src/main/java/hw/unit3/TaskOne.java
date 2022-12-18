178caded4764af73c2b3521608e3375ebb930413
alisasira <alisa.sira@gmail.com>
2022-12-18 23:44:28 +0200

    Find sum from string implementation

---
+++ unit_3_start_oop/src/main/java/hw/unit3/TaskOne.java
@@ -0,0 +1,33 @@
+package hw.unit3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class TaskOne {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Please, enter stupid combination of letters and numbers:");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = bf.readLine();
+
+        int sum = 0;
+        char[] chars = input.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            sum += parseDigit(chars[i]);
+        }
+
+        System.out.println("Your sum is: " + sum);
+    }
+
+    private static int parseDigit(char character) {
+        if (Character.isDigit(character)) {
+            int x = Character.digit(character, 10);
+            return x;
+        } else {
+            return 0;
+        }
+
+    }
+}
+
